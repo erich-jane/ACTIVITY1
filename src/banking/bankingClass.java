5f7d88305f4ed2e076bdbbb9398cd53b3fbcc3bf
erich-jane <canazareserichjane@gmail.com>
2025-08-19 13:20:54 +0800

    added NumberChecker

---
+++ src/banking/bankingClass.java
@@ -0,0 +1,49 @@
+package banking;
+
+public class bankingClass {
+    
+    private int accountNo;
+    private float initialBalance;
+    private int pin;
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    public boolean verifyAccount(int acc, int pn){
+        
+        return acc == accountNo && pn == pin;
+    }
+    
+    public int setAccount(){
+        return accountNo;
+    }
+    
+    public void viewBalance(){
+    
+    
+    }
+    
+    
+    
+}
